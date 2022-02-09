69931e9f4cffd515f260a354b301ed6d12f3f2a9
suy2on <tnwjdrms@naver.com>
2022-02-09 17:15:48 +0900

    # feat : 종속성 코드 분리

--- Dockerfile
+++ Dockerfile
@@ -2,10 +2,11 @@ FROM node:10
 
 WORKDIR /usr/src/app
 
-COPY ./ ./
+COPY package.json ./
 
 
 RUN npm install 
 
+COPY ./ ./
 
 CMD [ "node", "server.js" ]
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const PORT = 8080;
 //APP
 const app = express();
 app.get('/', (req,res) => {
-    res.send("Hello World")
+    res.send("안녕하세요!!!")
 });
 
 app.listen(PORT);
